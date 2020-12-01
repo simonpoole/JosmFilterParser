a3bc79371d96c9ccd5343dc87fe903f567f0176f
simonpoole <simon@poole.ch>
2020-12-01 16:58:36 +0100

    Organize imports

--- src/test/java/ch/poole/osm/josmfilterparser/JosmFilterIntegrationTest.java
+++ src/test/java/ch/poole/osm/josmfilterparser/JosmFilterIntegrationTest.java
@@ -1,20 +1,8 @@
 
 package ch.poole.osm.josmfilterparser;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
